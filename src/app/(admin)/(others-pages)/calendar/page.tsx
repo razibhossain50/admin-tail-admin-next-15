818f420553517ccc3e37dfbf14063a5dd9861fd2
razibhossain-hbk <ext_razib.hossain@hbkworld.com>
2025-02-14 14:48:59 +0600

    basic admin page initialize

---
+++ src/app/(admin)/(others-pages)/calendar/page.tsx
@@ -0,0 +1,17 @@
+import Calendar from "@/components/calendar/Calendar";
+import PageBreadcrumb from "@/components/common/PageBreadCrumb";
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Calender"
+  // other metadata
+};
+export default function page() {
+  return (
+    <div>
+      <PageBreadcrumb pageTitle="Calendar" />
+      <Calendar />
+    </div>
+  );
+}
---
+++ src/app/(admin)/(ui-elements)/videos/page.tsx
@@ -0,0 +1,18 @@
+import PageBreadcrumb from "@/components/common/PageBreadCrumb";
+import VideosExample from "@/components/ui/video/VideosExample";
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Videos template",
+};
+
+export default function VideoPage() {
+  return (
+    <div>
+      <PageBreadcrumb pageTitle="Videos" />
+
+      <VideosExample />
+    </div>
+  );
+}
